d02e7908a2e65b07d3c40efb5ba29ef91e1e16ab
Jason Yoo <43391992+JYoo1@users.noreply.github.com>
2021-06-28 13:31:04 -0700

    working product

--- vue-front-end/vue.config.js
+++ vue-front-end/vue.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8081',
-        ws: true,
-        changeOrigin: true
-      }
-    }
-  },
   configureWebpack: {
     devServer: {
       headers: { "Access-Control-Allow-Origin": "*" }
@@ -16,5 +7,5 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-front-end/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "./" : "/"
 }
