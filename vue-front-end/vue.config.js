a121524a9c1e340ed550d26ad8717f0d23697075
Jason Yoo <43391992+JYoo1@users.noreply.github.com>
2021-06-28 11:57:13 -0700

    test

--- vue-front-end/vue.config.js
+++ vue-front-end/vue.config.js
@@ -16,5 +16,5 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-leaflet-geolocation-selector/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/index.html" : "/"
 }
