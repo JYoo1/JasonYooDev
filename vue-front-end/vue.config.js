c168215308cfe53166109d0903dbbdce7607d78a
Jason Yoo <43391992+JYoo1@users.noreply.github.com>
2021-06-28 10:30:42 -0700

    test prod 4

--- vue-front-end/vue.config.js
+++ vue-front-end/vue.config.js
@@ -6,10 +6,7 @@ module.exports = {
         ws: true,
         changeOrigin: true
       }
-    },
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue-front-end/'
-    : '/'
+    }
   },
   configureWebpack: {
     devServer: {
